a56ddf5660d86c831b147bdfd11e9d818c082a96
jBachmanKelley <jBachmanKelley@gmail.com>
2020-09-30 16:25:35 -0400

    Update main.py
    Started scipt, need to replace usages of numpy methods, except those used for display

--- main.py
+++ main.py
@@ -1,16 +1,42 @@
-# John Kelley, 09/29/2020
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# John Kelley, 09/29/2020, CMSC 409: Artificial Intelligence, Milos Manic #
+import numpy as np
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class Perceptron:
+    # This method will train the perceptron: Max iterations are 5000, the error threshold should be passed in
+    def train(self, x, y, max_iterations=5000):
+        n_samples = x.shape[0]
+        n_features = x.shape[1]
+
+        # Add 1 for the bias term
+        self.weights = np.zeros((n_features + 1))
+
+        # Add column of 1s
+        x = np.concatenate([x, np.ones((n_samples, 1))], axis=1)
+
+        for i in range(max_iterations):
+            for j in range(n_samples):
+                if y[j] * np.dot(self.weights, x[j, :]) <= 0:
+                    self.weights += y[j] * x[j, :]
+
+    # This method will predict labels for passsed data and handle unlabeled data
+    def label(self, X):
+        if not hasattr(self, 'weights'):
+            print("The data hasn't been trained yet")
+            return
+
+        n_samples = X.shape[10]
+        # Add column of 1s
+        X = np.concatenate([X, np.ones((n_samples, 1))], axis=1)
+        y = np.matmul(X, self.weights)
+        y = np.vectorize(lambda val: 1 if val > 0 else -1)(y)
+
+        return y
+
+    # This method calculates the accuracy of our model using______
+    def assess(self, X, y):
+        pred_y = self.label(X)
+
+        return np.mean(y == pred_y)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
